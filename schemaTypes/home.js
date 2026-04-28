d54720f3afb7f7f2d45dd8abd26cbe8839dede1f
bashar35790 <bashar35790@gmail.com>
2026-04-28 18:26:24 +0600

    added home page demo section schema

--- schemaTypes/home.js
+++ schemaTypes/home.js
@@ -135,8 +135,8 @@ export default {
                   type: 'string',
                   options: {
                     list: [
-                      { title: 'Trending Up', value: 'TrendingUp' },
-                      { title: 'Trending Down', value: 'TrendingDown' },
+                      {title: 'TrendingUp', value: 'TrendingUp'},
+                      {title: 'TrendingDown', value: 'TrendingDown'},
                     ],
                   },
                   initialValue: 'TrendingUp',
@@ -145,8 +145,9 @@ export default {
                   name: 'logo',
                   title: 'Logo Image',
                   type: 'url',
-                  description: "Paste image URL here (e.g. https://example.com/logo.png)",
-                  initialValue: 'https://cdnwebsite.databox.com/wp-content/uploads/2024/01/25053353/First_Response_logo-1.png',
+                  description: 'Paste image URL here (e.g. https://example.com/logo.png)',
+                  initialValue:
+                    'https://cdnwebsite.databox.com/wp-content/uploads/2024/01/25053353/First_Response_logo-1.png',
                 },
               ],
             },
@@ -172,6 +173,119 @@ export default {
       ],
     },
 
+    // Demo section start here
+    {
+      name: 'demoSecton',
+      title: 'Home Demo Section',
+      type: 'object',
+      fields: [
+        {
+          name: 'blackHeading',
+          title: 'Demo Heading  First Part',
+          type: 'string',
+          initialValue: 'Get answers from your data',
+        },
 
+        {
+          name: 'colorHeading',
+          title: 'Heading Secound Part',
+          type: 'string',
+          initialValue: 'instantly',
+        },
+        {
+          name: 'subHeading',
+          title: 'Sub Headinag',
+          type: 'string',
+          initialValue:
+            'Getting answers from your data used to take hours. Not anymore. With Genie, our AI analyst, you can ask a question and get a clear answer in seconds.',
+        },
+        {
+          name: 'img',
+          title: 'Demo Dashboard Image',
+          type: 'image',
+          options: {
+            hotspot: true,
+            initialValue:"dashboard"
+          },
+        },
+
+        {
+          name: 'Feature1',
+          title: 'Feature 1 Card',
+          type: 'object',
+          fields: [
+            {
+              name: 'boldText',
+              title: 'Bold Text',
+              type: 'string',
+              initialValue: 'Ask',
+            },
+            {
+              name: 'lightText',
+              title: 'Light Text',
+              type: 'string',
+              initialValue: 'business performance questions in plain language',
+            },
+          ],
+        },
+        {
+          name: 'Feature2',
+          title: 'Feature 2 Card',
+          type: 'object',
+          fields: [
+            {
+              name: 'boldText',
+              title: 'Bold Text',
+              type: 'string',
+              initialValue: 'Create',
+            },
+            {
+              name: 'lightText',
+              title: 'Light Text',
+              type: 'string',
+              initialValue: 'metrics and dashboards with a prompt',
+            },
+          ],
+        },
+        {
+          name: 'Feature3',
+          title: 'Feature 3 Card',
+          type: 'object',
+          fields: [
+            {
+              name: 'boldText',
+              title: 'Bold Text',
+              type: 'string',
+              initialValue: 'Understand',
+            },
+            {
+              name: 'lightText',
+              title: 'Light Text',
+              type: 'string',
+              initialValue: "what's driving changes in your metrics",
+            },
+          ],
+        },
+        {
+          name: 'Feature4',
+          title: 'Feature 4 Card',
+          type: 'object',
+          fields: [
+            {
+              name: 'boldText',
+              title: 'Bold Text',
+              type: 'string',
+              initialValue: 'Train',
+            },
+            {
+              name: 'lightText',
+              title: 'Light Text',
+              type: 'string',
+              initialValue: "Genie your business context",
+            },
+          ],
+        },
+      ],
+    },
   ],
 }
